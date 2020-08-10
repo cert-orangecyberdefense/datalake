6e4d35be6034dcdb83778d78cd8e7fe628e43a0a
Hugo CHASTEL <hugo.chastel@orange.com>
2020-08-10 18:31:03 +0200

    MRTI-2681 # Bumping version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='datalake-scripts',
-    version='1.23.0',
+    version='1.23.1',
     author='OCD',
     author_email='datalake-interne.ocd@orange.com',
     description='A collection of scripts to easily use the API of OCD Datalake',
