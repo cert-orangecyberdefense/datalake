32f78fbb35ef23edc38b67a7615264f1451ccaef
Hugo CHASTEL <hugo.chastel@orange.com>
2021-02-03 17:30:51 +0100

    MRTI-3163 # Add Advanced search endpoint
    Bump package version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='datalake-scripts',
-    version='2.1.1',
+    version='2.2.0',
     author='OCD',
     author_email='cert-contact.ocd@orange.com',
     description='A collection of scripts to easily use the API of OCD Datalake',
