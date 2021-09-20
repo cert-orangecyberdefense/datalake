ecfdf679ce3778eca4b9882ba685b78e9d549366
Hugo CHASTEL <hugo.chastel@orange.com>
2021-09-20 14:22:33 +0200

    MRTI-3777 # Set configurable loglevel
    For internal debug as of now

--- datalake/datalake.py
+++ datalake/datalake.py
@@ -1,5 +1,6 @@
 import logging
 
+from datalake import AtomType
 from datalake.common.base_script import BaseScripts
 from datalake.scripts.threats import Threats
 from datalake.scripts.bulk_search import BulkSearch
@@ -19,11 +20,13 @@ class Datalake:
     >>> dtl.Threats.lookup(atom_value='mayoclinic.org', atom_type=AtomType.DOMAIN, hashkey_only=False)
     """
 
+    LOG_LEVEL = logging.WARNING
+
     def __init__(self, username: str = None, password: str = None, env='prod'):
         self.username = username
         self.password = password
         self.env = env
-        args = ConfigArg(loglevel=logging.WARNING, env=env)
+        args = ConfigArg(loglevel=self.LOG_LEVEL, env=env)
         self.starter = BaseScripts()
         endpoint_config, _, tokens = self.starter.load_config(args=args, username=self.username, password=self.password)
         self.tokens = tokens
