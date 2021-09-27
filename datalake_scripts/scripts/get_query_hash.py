b62582606fae629371b749cb2e651afc906c9d1b
Hugo CHASTEL <hugo.chastel@orange.com>
2021-09-27 18:36:21 +0200

    MRTI-3777 # Fix get_query_hash

--- datalake_scripts/scripts/get_query_hash.py
+++ datalake_scripts/scripts/get_query_hash.py
@@ -1,17 +1,18 @@
 import json
 import sys
 
+from datalake.common.config import Config
+from datalake.common.logger import logger, configure_logging
+from datalake.common.token_manager import TokenManager
 from datalake_scripts.common.base_script import BaseScripts
-from datalake_scripts.common.logger import logger
 from datalake_scripts.engines.post_engine import AdvancedSearch
 
 
 def main(override_args=None):
-    starter = BaseScripts()
     logger.debug(f'START: get_query_hash.py')
 
     # Load initial args
-    parser = starter.start('Retrieve a query hash from a query body (a json used for the Advanced Search).')
+    parser = BaseScripts.start('Retrieve a query hash from a query body (a json used for the Advanced Search).')
     required_named = parser.add_argument_group('required arguments')
     required_named.add_argument(
         'query_body_path',
@@ -21,14 +22,16 @@ def main(override_args=None):
         args = parser.parse_args(override_args)
     else:
         args = parser.parse_args()
+    configure_logging(args.loglevel)
 
     # Load api_endpoints and tokens
-    endpoint_config, main_url, tokens = starter.load_config(args)
+    endpoint_config = Config().load_config()
+    token_manager = TokenManager(endpoint_config, environment=args.env)
     with open(args.query_body_path, 'r') as query_body_file:
         query_body = json.load(query_body_file)
     logger.debug(f'Retrieving query hash for query body: {query_body}')
 
-    advanced_search = AdvancedSearch(endpoint_config, args.env, tokens)
+    advanced_search = AdvancedSearch(endpoint_config, args.env, token_manager)
 
     response = advanced_search.get_threats(query_body, limit=0)
     if not response or 'query_hash' not in response:
