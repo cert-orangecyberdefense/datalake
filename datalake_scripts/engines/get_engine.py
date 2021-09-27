1eb6db8e50938588e92853345e14c88e8e043499
Hugo CHASTEL <hugo.chastel@orange.com>
2021-09-27 18:34:33 +0200

    MRTI-3777 # Fix get_threats_by_hashkey.py

--- datalake_scripts/engines/get_engine.py
+++ datalake_scripts/engines/get_engine.py
@@ -3,6 +3,7 @@
 from requests import PreparedRequest
 
 from datalake.common.logger import logger
+from datalake.endpoints import Endpoint
 from datalake_scripts.common.base_engine import BaseEngine
 
 
@@ -28,6 +29,7 @@ class ThreatsSearch(GetEngine):
         Retrieve the JSON file of a list of threats and their comments.
         :return dict, list: threats found and a list of hashkey not found
         """
+        terminal_size = Endpoint._get_terminal_size()
         total_hash = len(list_threats)
         dict_threat = {'count': total_hash, 'results': []}
         list_of_lost_hashes = []
@@ -42,9 +44,9 @@ class ThreatsSearch(GetEngine):
             final_dict = response_dict
             if not response_dict.get('hashkey'):
                 list_of_lost_hashes.append(threat)
-                logger.info(f'{str(index).ljust(5)}:{threat.ljust(self.terminal_size - 11)}\x1b[0;30;41mERROR\x1b[0m')
+                logger.info(f'{str(index).ljust(5)}:{threat.ljust(terminal_size - 11)}\x1b[0;30;41mERROR\x1b[0m')
             else:
-                logger.info(f'{str(index).ljust(5)}:{threat.ljust(self.terminal_size - 10)}\x1b[0;30;42m OK \x1b[0m')
+                logger.info(f'{str(index).ljust(5)}:{threat.ljust(terminal_size - 10)}\x1b[0;30;42m OK \x1b[0m')
                 response_dict = self.datalake_requests(f'{request_url}/comments', 'get', self._get_headers(), None)
                 final_dict['comments'] = response_dict
                 dict_threat['results'].append(final_dict)
--- datalake_scripts/scripts/get_threats_by_hashkey.py
+++ datalake_scripts/scripts/get_threats_by_hashkey.py
@@ -1,17 +1,19 @@
 import sys
 
+from datalake.common.config import Config
+from datalake.common.logger import logger, configure_logging
+from datalake.common.token_manager import TokenManager
 from datalake_scripts.common.base_script import BaseScripts
-from datalake_scripts.common.logger import logger
 from datalake_scripts.engines.get_engine import ThreatsSearch
+from datalake_scripts.helper_scripts.utils import load_list, save_output
 
 
 def main(override_args=None):
     """Method to start the script"""
-    starter = BaseScripts()
     logger.debug(f'START: get_threats_by_hashkey.py')
 
     # Load initial args
-    parser = starter.start('Retrieve threats (as Json) from a list of ids (hashkeys)')
+    parser = BaseScripts.start('Retrieve threats (as Json) from a list of ids (hashkeys)')
     parser.add_argument(
         'hashkeys',
         help='hashkeys of the threats to retreive',
@@ -30,22 +32,24 @@ def main(override_args=None):
         args = parser.parse_args(override_args)
     else:
         args = parser.parse_args()
+    configure_logging(args.loglevel)
 
     if not args.hashkeys and not args.input_file:
         parser.error("either a hashkey or an input_file is required")
-    threats_list = starter._load_list(args.input_file) if args.input_file else args.hashkeys
+    threats_list = load_list(args.input_file) if args.input_file else args.hashkeys
     logger.debug(f'TOTAL: {len(threats_list)} threats found')
 
     # Load api_endpoints and tokens
-    endpoint_config, main_url, tokens = starter.load_config(args)
-    search_engine_threats = ThreatsSearch(endpoint_config, args.env, tokens)
+    endpoint_config = Config().load_config()
+    token_manager = TokenManager(endpoint_config, environment=args.env)
+    search_engine_threats = ThreatsSearch(endpoint_config, args.env, token_manager)
     list_threats, list_lost_hashes = search_engine_threats.get_json(threats_list)
 
     if args.output:
-        starter.save_output(args.output, list_threats)
+        save_output(args.output, list_threats)
         logger.debug(f'Threats JSON saved in {args.output}\n')
     if args.lost:
-        starter.save_output(args.lost, list_lost_hashes)
+        save_output(args.lost, list_lost_hashes)
         logger.debug(f'Threats lost saved in {args.lost}\n')
     logger.debug(f'END: get_threats_by_hashkey.py')
 
