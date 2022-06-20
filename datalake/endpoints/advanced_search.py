b942e8e1a0252ec8abc4e24edefba8f9167210b6
Martin <91955997+mrtnhwtt@users.noreply.github.com>
2022-06-20 11:49:05 +0200

    #56 Advanced_search_from_query_body: wrong type for ordering (#57)
    * bugfix/GH-56# Fixed mistake in tutorial.md section for advanced search,
    Fixed mistake in ordering list available options and corrected the variable type sent to the API
    Updated advanced_search to receive a list for ordering for consistency with the API,
    Updated tests and documentation accordingly
    Updated the CLI to the new argument type for ordering

--- datalake/endpoints/advanced_search.py
+++ datalake/endpoints/advanced_search.py
@@ -1,19 +1,22 @@
 from datalake.endpoints.endpoint import Endpoint
 from datalake.common.ouput import parse_response, Output, output_supported
 from requests.sessions import PreparedRequest
+from typing import List
 
 
 class AdvancedSearch(Endpoint):
-    ordering_list = ['first_seen', '-first-seen', 'last_updated', '-last_updated', 'events_count', '-events_count',
+    ordering_list = ['first_seen', '-first_seen', 'last_updated', '-last_updated', 'events_count', '-events_count',
                      'sources_count', '-sources_count']
 
     @output_supported({Output.JSON, Output.STIX, Output.MISP, Output.CSV})
     def advanced_search_from_query_body(self, query_body: dict, limit: int = 20, offset: int = 0, 
-                                        ordering=None, output=Output.JSON):
+                                        ordering: List[str] = None, output=Output.JSON):
         if not query_body:
             raise ValueError("query_body is required")
-        if ordering and ordering not in self.ordering_list:
-            raise ValueError(f'ordering needs to be one of the following str : {", ".join(self.ordering_list)}')
+        if ordering and not set(ordering).issubset(self.ordering_list):
+            raise ValueError(
+                f'ordering needs to be a list of at least one of the following str : {", ".join(self.ordering_list)}'
+            )
         body = {
             'query_body': query_body,
             'limit': limit,
@@ -27,13 +30,17 @@ class AdvancedSearch(Endpoint):
 
     @output_supported({Output.JSON, Output.STIX, Output.MISP, Output.CSV})
     def advanced_search_from_query_hash(self, query_hash, limit: int = 20, offset: int = 0,
-                                        ordering=None, output=Output.JSON):
+                                        ordering: List[str] = None, output=Output.JSON):
         if not query_hash:
             raise ValueError("query_hash is required")
-        if ordering and ordering not in self.ordering_list:
-            raise ValueError(f'ordering needs to be one of the following str : {", ".join(self.ordering_list)}')
+        if ordering and not set(ordering).issubset(self.ordering_list):
+            raise ValueError(
+                f'ordering needs to be a list of at least one of the following str : {", ".join(self.ordering_list)}'
+            )
         url = self._build_url_for_endpoint('advanced-search-hash').format(query_hash=query_hash)
-        params = {'limit': limit, 'offset': offset, 'ordering': ordering}
+        params = {'limit': limit, 'offset': offset}
+        if ordering:
+            params['ordering'] = ordering
         req = PreparedRequest()
         req.prepare_url(url, params)
         url = req.url
