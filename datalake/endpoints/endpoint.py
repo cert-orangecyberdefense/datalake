7f7d7a6656758645513aae22fbd5c3c50763b1aa
Martin <martin.hewitt@orange.com>
2022-09-26 15:33:07 +0200

    MRTI-4753 # Updated behavior for 422 code response from the API

--- datalake/endpoints/endpoint.py
+++ datalake/endpoints/endpoint.py
@@ -70,9 +70,15 @@ class Endpoint:
                 logger.warning('Token expired or Missing authorization header. Updating token')
                 self.token_manager.process_auth_error(response.json().get('msg'))
             elif response.status_code == 422:
-                logger.warning('Bad authorization header. Updating token')
-                logger.debug(f'422 HTTP code: {response.text}')
-                self.token_manager.process_auth_error(response.json().get('messages'))
+                if "msg" in response.json():
+                    logger.error(f'Error message: {response.json().get("msg")}')
+                elif "message" in response.json():
+                    logger.error(f'Error message: {response.json().get("message")}')
+                elif "messages" in response.json():
+                    logger.error(f'Error message: {response.json().get("messages")}')
+                else:
+                    logger.error(f'422 HTTP code: {response.text}')
+                break
             elif response.status_code < 200 or response.status_code > 299:
                 logger.error(
                     f'API returned non 2xx response code : {response.status_code}\n{response.text}\n Retrying'
