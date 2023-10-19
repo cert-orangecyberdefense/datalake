28cce44c83472a62bff222efd3ef2d881de40852
ClaireOrange <claire.perret@orange.com>
2023-10-19 15:57:17 +0200

    v2.6.9

--- datalake_scripts/cli.py
+++ datalake_scripts/cli.py
@@ -9,7 +9,7 @@ from datalake_scripts.scripts import add_threats, get_threats_by_hashkey, edit_s
 
 class Cli:
     CLI_NAME = "ocd-dtl"
-    VERSION = "2.6.8"
+    VERSION = "2.6.9"
 
     def __init__(self):
         parser = argparse.ArgumentParser(
