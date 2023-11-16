eca403be5c85bfaf9ba22ae4e372c235f7d852d2
ClaireOrange <claire.perret@orange.com>
2023-11-16 18:41:53 +0100

    MRTI-5554 # add tests for cli calls and update doc files

---
+++ tests/scripts/test_cli.py
@@ -0,0 +1,29 @@
+import sys
+import pytest
+from unittest.mock import patch
+from datalake_scripts.cli import Cli
+from datalake_scripts.scripts import add_threats, get_threats_by_hashkey, edit_score, get_threats_from_query_hash, \
+    add_comments, lookup_threats, add_tags, get_query_hash, bulk_lookup_threats, advanced_search, get_atom_values
+
+
+# Tests to make sure the commands call the right scripts (and recognizes the command)
+@pytest.mark.parametrize("function_script", [add_threats, get_threats_by_hashkey, edit_score, get_threats_from_query_hash, \
+    add_comments, lookup_threats, add_tags, get_query_hash, bulk_lookup_threats, advanced_search, get_atom_values])
+def test_name_function(function_script):
+    command_name = function_script.__name__.split('.')[-1]
+
+    # Special cases in cli.py:
+    if command_name == "add_comments":
+        command_name = "add_comment"
+    elif command_name == "get_threats_by_hashkey":
+        command_name = "get_threats"
+
+    with patch.object(function_script, 'main', return_value='mocked response') as mock_function_script:
+        # Act: Change sys.argv to simulate the command line arguments
+        with patch('sys.argv', ['ocd-dtl', command_name, '--output', 'output.txt', '--env', 'preprod']):
+            cli_app = Cli()
+        
+        # Assert: ensure the mock was called and the result is as expected
+        mock_function_script.assert_called_once_with(['--output', 'output.txt', '--env', 'preprod'])
+
+
