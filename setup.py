9a79a2163d16301381559976894860dee12458c8
ClaireOrange <claire.perret@orange.com>
2025-12-17 10:59:51 +0100

    MRTI-7429 # PEP 517 - remove import in setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,31 @@ import os
 
 from setuptools import find_packages, setup
 
-from datalake_scripts.cli import Cli
-
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
+
+
+def read_version_or_cli_name(option: str):
+    with open("datalake_scripts/cli.py") as f:
+        for line in f:
+            if option == "version" and line.strip().startswith("VERSION"):
+                return line.split("=")[1].strip().strip('"')
+            if option == "cli_name" and line.strip().startswith("CLI_NAME"):
+                return line.split("=")[1].strip().strip('"')
+
+
+CLI_NAME = read_version_or_cli_name("cli_name")
+VERSION = read_version_or_cli_name("version")
 
 setup(
     name="datalake_scripts",
-    version=Cli.VERSION,
+    version=VERSION,
     author="OCD",
     author_email="cert-contact.ocd@orange.com",
     description="A collection of scripts to easily use the API of OCD Datalake",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/cert-orangecyberdefense/datalake/",
     package_dir={
@@ -25,7 +36,7 @@ setup(
     install_requires=["requests", "halo", "prettytable"],
     test_suite="nose.collector",
     entry_points={
-        "console_scripts": (f"{Cli.CLI_NAME} = datalake_scripts.cli:main",),
+        "console_scripts": (f"{CLI_NAME} = datalake_scripts.cli:main",),
     },
     classifiers=[
         "Programming Language :: Python :: 3.7",
